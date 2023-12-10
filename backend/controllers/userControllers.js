0046107952e125c1d92f857713c8971a1a6cfa4c
mahmoud-batman <mahmoud148043@gmail.com>
2023-12-10 16:04:14 +0200

    create user register route, controller, model

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,10 +1,42 @@
+const User = require("../models/userModel");
+const asyncHandler = require("express-async-handler");
+
 const getAllUsers = (req, res) => {
   res.send("getAllUsers");
 };
 
-const registerUser = (req, res, next) => {
-  const { name } = req.body;
-};
+const registerUser = asyncHandler(async (req, res, next) => {
+  const { name, password } = req.body;
+
+  // Validation
+  if (!name || !password) {
+    res.status(400);
+    throw new Error("Please fill in all required fields");
+  }
+
+  if (password.toString().length < 6) {
+    res.status(400);
+    throw new Error("Password must be up to 6 characters");
+  }
+
+  // Check if user email already exists
+  const userExists = await User.findOne({ name });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("name has already been registered");
+  }
+
+  const user = await User.create({ name, password });
+  if (!user) {
+    res.status(500);
+    throw new Error("server error");
+  } else {
+    const { _id, name, photo, password } = user;
+    console.log(password);
+    res.send({ _id, name, photo, password });
+  }
+});
 
 module.exports = {
   registerUser,
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema(
   {
@@ -22,6 +23,19 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function (next) {
+  // if password is not modified continue to the next middleware
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  // hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(this.password, salt);
+  this.password = hashedPassword;
+  next();
+});
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
